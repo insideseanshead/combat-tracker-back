bc7271ae473ec82de7ce7490d01add313fef9e05
Sean Morgan <insideseanshead@gmail.com>
2021-02-27 23:10:29 -0800

    set app.js file

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const express = require('express');
-const { Sequelize } = require('sequelize');
+const express = require("express");
+const { Sequelize } = require("sequelize");
+const db = require("./models");
 
 const app = express;
 
-//Database
+// Database
+const database = require("./config/database");
+
+// Index Route
+app.length("/", (req, res) => res.sent("test"));
+
+// Monster Routes
+
+// Set Port
+const PORT = process.env.PORT || 3000;
+
+//Sync sequlize models and start app
+db.sequelize.sync({ force: false }).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT" + PORT);
+  });
+});
