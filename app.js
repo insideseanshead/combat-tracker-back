1df2e84664e302df8ab8e30b9f154cd8e479a7fc
Sean Morgan <insideseanshead@gmail.com>
2021-03-16 20:59:49 -0700

    refactored app.js added user.js to models

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const db = require("./models");
-const cors = require('cors')
+// const cors = require('cors')
 
 // const routes = require('./routes')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const allRoutes = require('./controllers')
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
-app.use(cors())
+// app.use(cors())
 
 // Routes
-const routes = require("./controllers/")
-app.use(routes);
+// const routes = require("./controllers/")
+// app.use(routes);
+app.use('/', allRoutes)
 
 //Sync sequlize models and start app
-db.sequelize.sync({ force: false }).then(function () {
+db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT" + PORT);
   });
