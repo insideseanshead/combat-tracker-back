1776e7671a795388da9b82117cd88aa34ecab361
Sean Morgan <insideseanshead@gmail.com>
2021-03-18 23:12:56 -0700

    created campaign controller can now add campaign to users

---
+++ controllers/campaignController.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var router = express.Router();
+const jwt = require('jsonwebtoken');
+const db = require("../models");
+
+const checkAuthStatus = request => {
+    if(!request.headers.authorization) {
+        return false
+    }
+    
+    token = request.headers.authorization.split(' ')[1]
+    console.log(token);
+    const loggedInUser = jwt.verify(token, 'secretString', (err,data) =>{
+        if(err) {
+            return false
+        }
+        else {
+            return data
+        }
+    });
+    console.log(loggedInUser)
+    return loggedInUser
+}
+
+router.get("/",(req,res)=>{
+    db.Campaign.findAll().then(campaigns=>{
+        res.json(campaigns);
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).send("something went wrong")
+    })
+});
+
+router.post('/',(req,res)=>{
+    const loggedInUser = checkAuthStatus(req);
+    if(!loggedInUser){
+        return res.status(401).send('Please log in')
+    }
+    console.log(loggedInUser);
+    db.Campaign.create({
+        name:req.body.name,
+        UserId:loggedInUser.id
+    }).then(newCampaign=>{
+        res.json(newCampaign)
+    }).catch(err=>{
+        console.log(err)
+        res.status(500).send('something went wrong')
+    })
+})
+
+
+module.exports = router
--- controllers/index.js
+++ controllers/index.js
@@ -3,12 +3,14 @@ var router = express.Router();
 
 const monsterRoutes = require("./monsterRoutes");
 const userRoutes=require('./userController')
+const campaignRoutes=require('./campaignController')
 
 router.get("/",(req,res)=>{
     res.send("Monster time")
 });
 
 router.use('/api/users', userRoutes)
+router.use('/api/campaign', campaignRoutes)
 router.use("/api/monsters", monsterRoutes)
 
 module.exports = router
