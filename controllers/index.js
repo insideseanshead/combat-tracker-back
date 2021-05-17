50cf2ee40afb6ee5195b7fd2ab258440d7017843
Sean Morgan <insideseanshead@gmail.com>
2021-05-17 11:53:26 -0700

    added characterRoutes

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,7 @@ const monsterRoutes = require("./monsterRoutes");
 const userRoutes = require("./userController");
 const campaignRoutes = require("./campaignController");
 const encounterRoutes = require("./encounterController");
+const characterRoutes = require('./characterController');
 
 router.get("/", (req, res) => {
   res.send("Monster time");
@@ -14,5 +15,6 @@ router.use("/api/users", userRoutes);
 router.use("/api/campaigns", campaignRoutes);
 router.use("/api/encounters", encounterRoutes);
 router.use("/api/monsters", monsterRoutes);
+router.use('/api/characters', characterRoutes);
 
 module.exports = router;
